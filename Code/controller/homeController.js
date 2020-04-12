4674dd2cb3ae4d2f044c075c95b5399858f32b8c
davi-barbosa <davi199858@gmail.com>
2020-04-12 20:09:05 -0300

    Adicionando a pagina home

---
+++ Code/controller/homeController.js
@@ -0,0 +1,6 @@
+
+const homeController = function(req, res){
+    res.render('home/home');
+}
+
+module.exports = homeController;
--- Code/routes/index.js
+++ Code/routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var router = express.Router();
 var { Login } = require('../controller/login')
 const artigoRoute = require('./artigos/artigoRoute')
+const homeRoute = require('./home/homeRoute')
 
 
 router.get('/login', Login);
 
 router.use('/artigos', artigoRoute)
+
+router.use('/', homeRoute);
 
 router.get('/', function (req, res){
     res.send('Server is running...');
