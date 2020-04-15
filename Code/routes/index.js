87cadda4564898f36b05bbd08411334dda985a09
Augusto Gomes <augusto.silva@elaw.com.br>
2020-04-14 22:39:36 -0300

    Telas de Login e cadastro de Usuários

--- Code/routes/index.js
+++ Code/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var { Login } = require('../controller/login')
 const artigoRoute = require('./artigos/artigoRoute')
 const homeRoute = require('./home/homeRoute')
+const userRoute = require('./usuarios/usuarioRoute')
 
 
 router.get('/login', Login);
@@ -14,6 +15,8 @@ router.use('/', homeRoute);
 router.get('/', function (req, res){
     res.send('Server is running...');
 })
+
+router.use('/usuarios', userRoute);
 
 module.exports = {
     router
