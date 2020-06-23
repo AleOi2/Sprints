ff9739f3262c2563daf90579e340e2fbd01182fe
Dario <dario.aqn@gmail.com>
2020-06-22 23:42:48 -0300

    pasta sprints-sql com scripts para colocar  dados nas tabelas

---
+++ sql/dadosCategory.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM santander.category
+LIMIT 0, 1000
+
+-- Date: 2020-06-22 23:39
+*/
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (1,'educacao.png','Educação','D',100.00,'2020-05-19 19:00:20','2020-05-19 19:00:24');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (2,'emprestimor.png','Empréstimo(R)','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:01:01');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (3,'lazer.png','Lazer','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (4,'mercado.png','Mercado','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (5,'moradia.png','Moradia','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (6,'restaurante.png','Restaurante','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (7,'saude.png','Saúde','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (8,'servicos.png','Serviços','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (9,'transporte.png','Transporte','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (10,'vestuario.png','Vestuário','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (11,'viagens.png','Viagens','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (12,'outrasd.png','Outras Despezas','D',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (13,'salario.png','Salário','R',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (14,'emprestimod.png','Empréstimo(D)','R',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
+INSERT INTO `` (`id`,`category`,`label`,`type`,`valuePredict`,`createdAt`,`updatedAt`) VALUES (15,'outrasr.png','Outras Receitas','R',100.00,'2020-05-19 19:00:58','2020-05-19 19:00:58');
---
+++ sql/dadosRelease.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM santander.release
+LIMIT 0, 1000
+
+-- Date: 2020-06-22 23:40
+*/
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (1,'2020-06-22 00:00:00','escola',100.00,'2020-06-23 00:04:39','2020-06-23 00:04:49','2020-06',1,1);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (2,'2020-06-22 00:00:00','salário',1000.00,'2020-06-23 00:04:39','2020-06-23 00:04:49','2020-06',1,13);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (3,'2020-06-22 00:00:00','cinema',40.00,'2020-06-23 00:10:18','2020-06-23 00:10:18','2020-06',1,3);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (4,'2020-06-22 00:00:00','adiantamento',150.00,'2020-06-23 00:18:22','2020-06-23 00:18:22','2020-06',1,13);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (5,'2020-06-22 00:00:00','empréstimo',777.00,'2020-06-23 00:18:57','2020-06-23 00:18:57','2020-06',1,2);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (6,'2020-06-22 00:00:00','freela 2',55.00,'2020-06-23 00:19:10','2020-06-23 02:05:35','2020-06',1,15);
+INSERT INTO `` (`id`,`date`,`description`,`value`,`createdAt`,`updatedAt`,`month_year`,`users_id`,`category_id`) VALUES (7,'2020-06-22 00:00:00','escola',150.00,'2020-06-23 02:00:50','2020-06-23 02:00:50','2020-06',1,1);
